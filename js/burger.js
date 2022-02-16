2f6bc7be5c4dc383e70f395a4fe653130b19a8bb
SoulOfDarknes <ve4nuy@rambler.ru>
2022-02-16 23:16:14 +0200

    add transform to burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 (function () {
     const iconBurger = document.querySelector(".burger__menu");
-    const navigationsBody = document.querySelector(".header__nav-social");
+  const navigationsBody = document.querySelector(".header__nav-social");
     iconBurger.addEventListener("click", function (e) {
       document.body.classList.toggle("lock");
       iconBurger.classList.toggle("active");
@@ -13,9 +13,9 @@
             if (iconBurger.classList.contains("active")) {
                 document.body.classList.remove("lock");
                 iconBurger.classList.remove("active");
-                navigationsBody.classList.remove("active");
+              navigationsBody.classList.remove("active");
             }
           }
       );
     });
-  })();
+})();
